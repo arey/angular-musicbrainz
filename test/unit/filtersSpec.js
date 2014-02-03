f0439257fb06a78d8c4154646524f90fa12d8ce1
Antoine Rey <antoine.rey@gmail.com>
2014-02-03 19:17:23 +0100

    Format unit test to be compliant with jshint rules

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -2,76 +2,77 @@
 
 /* jasmine specs for filters go here */
 
-describe('filter', function() {
-  beforeEach(module('musicAlbumApp.filters'));
+describe('filter', function () {
+    beforeEach(module('musicAlbumApp.filters'));
 
 
-  describe('interpolate', function() {
-    beforeEach(module(function($provide) {
-      $provide.value('version', 'TEST_VER');
-    }));
-
-
-    it('should replace VERSION', inject(function(interpolateFilter) {
-      expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
-    }));
-  });
-
-    describe('joinBy', function() {
-        it('join 2 tags with default delimiter', inject(function(joinByFilter) {
-            expect(joinByFilter(["rock", "blues"])).toEqual('rock, blues');
+    describe('interpolate', function () {
+        beforeEach(module(function ($provide) {
+            $provide.value('version', 'TEST_VER');
         }));
-        it('join 2 tags with custom delimiter', inject(function(joinByFilter) {
-            expect(joinByFilter(["rock", "blues"], " - ")).toEqual('rock - blues');
+
+
+        it('should replace VERSION', inject(function (interpolateFilter) {
+            expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
         }));
-        it('join a single tag', inject(function(joinByFilter) {
-            expect(joinByFilter(["rock"])).toEqual('rock');
+    });
+
+    describe('joinBy', function () {
+        it('join 2 tags with default delimiter', inject(function (joinByFilter) {
+            expect(joinByFilter(['rock', 'blues'])).toEqual('rock, blues');
         }));
-        it('join empty tag arrays', inject(function(joinByFilter) {
+        it('join 2 tags with custom delimiter', inject(function (joinByFilter) {
+            expect(joinByFilter(['rock', 'blues'], ' - ')).toEqual('rock - blues');
+        }));
+        it('join a single tag', inject(function (joinByFilter) {
+            expect(joinByFilter(['rock'])).toEqual('rock');
+        }));
+        it('join empty tag arrays', inject(function (joinByFilter) {
             expect(joinByFilter([])).toEqual('');
         }));
     });
 
-    describe('reverse', function() {
-        it('array of strings', inject(function(reverseFilter) {
-            expect(reverseFilter(["rock", "blues"])).toEqual(["blues", "rock"]);
-        }))
-        it('undefined items', inject(function(reverseFilter) {
+    describe('reverse', function () {
+        it('array of strings', inject(function (reverseFilter) {
+            expect(reverseFilter(['rock', 'blues'])).toEqual(['blues', 'rock']);
+        }));
+
+        it('undefined items', inject(function (reverseFilter) {
             expect(reverseFilter(undefined)).toEqual(undefined);
         }));
     });
 
-    describe('artistTypeLabel', function() {
+    describe('artistTypeLabel', function () {
         beforeEach(inject(function ($rootScope) {
             $rootScope.translation = {
-                "ARTIST_TYPE_1" : "Person"
+                'ARTIST_TYPE_1': 'Person'
             };
         }));
-        it('artist type 1', inject(function(artistTypeLabelFilter) {
+        it('artist type 1', inject(function (artistTypeLabelFilter) {
             expect(artistTypeLabelFilter(1)).toEqual('Person');
-        }))
+        }));
     });
 
-    describe('yearFormat', function() {
+    describe('yearFormat', function () {
         beforeEach(inject(function ($rootScope) {
             $rootScope.translation = {
-                "FILTER_RELEASE_DATE_BEFORE" : "Before",
-                "FILTER_RELEASE_DATE_SINCE" : "Since",
-                "FILTER_RELEASE_DATE_YEAR" : "Years"
+                'FILTER_RELEASE_DATE_BEFORE': 'Before',
+                'FILTER_RELEASE_DATE_SINCE': 'Since',
+                'FILTER_RELEASE_DATE_YEAR': 'Years'
             };
         }));
-        it('to 1970', inject(function(yearFormatFilter) {
-            expect(yearFormatFilter({ to : 1970})).toEqual('Before 1970');
-        }))
-        it('from 1970 to 1980', inject(function(yearFormatFilter) {
-            expect(yearFormatFilter({ from : 1970, to : 1900})).toEqual('Years 70');
-        }))
-        it('from 2000 to 2010', inject(function(yearFormatFilter) {
-            expect(yearFormatFilter({ from : 2000, to : 2010})).toEqual('Years 2000');
-        }))
-        it('from 2010', inject(function(yearFormatFilter) {
-            expect(yearFormatFilter({ from : 2010})).toEqual('Since 2010');
-        }))
+        it('to 1970', inject(function (yearFormatFilter) {
+            expect(yearFormatFilter({ to: 1970})).toEqual('Before 1970');
+        }));
+        it('from 1970 to 1980', inject(function (yearFormatFilter) {
+            expect(yearFormatFilter({ from: 1970, to: 1900})).toEqual('Years 70');
+        }));
+        it('from 2000 to 2010', inject(function (yearFormatFilter) {
+            expect(yearFormatFilter({ from: 2000, to: 2010})).toEqual('Years 2000');
+        }));
+        it('from 2010', inject(function (yearFormatFilter) {
+            expect(yearFormatFilter({ from: 2010})).toEqual('Since 2010');
+        }));
     });
 
 });
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,46 +2,46 @@
 
 /* jasmine specs for services go here */
 
-describe('service', function() {
-  beforeEach(module('musicAlbumApp.services'));
+describe('service', function () {
+    beforeEach(module('musicAlbumApp.services'));
 
 
-  describe('version service', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('1.0');
-    }));
-  });
+    describe('version service', function () {
+        it('should return current version', inject(function (version) {
+            expect(version).toEqual('1.0');
+        }));
+    });
 
-    describe('userLanguage service getFirstLanguageRange function', function() {
-        it('should return undefined language range', inject(function(userLanguage) {
+    describe('userLanguage service getFirstLanguageRange function', function () {
+        it('should return undefined language range', inject(function (userLanguage) {
             var langRange = userLanguage.getFirstLanguageRange();
             expect(langRange).toEqual(undefined);
         }));
-        it('should return en-us as first language range', inject(function(userLanguage) {
-            var langRange = userLanguage.getFirstLanguageRange("en-us,en,fr");
+        it('should return en-us as first language range', inject(function (userLanguage) {
+            var langRange = userLanguage.getFirstLanguageRange('en-us,en,fr');
             expect(langRange).toEqual('en-us');
         }));
-        it('should return en-us as first language range', inject(function(userLanguage) {
-            var langRange = userLanguage.getFirstLanguageRange("en-us;q=0.8,fr");
+        it('should return en-us as first language range', inject(function (userLanguage) {
+            var langRange = userLanguage.getFirstLanguageRange('en-us;q=0.8,fr');
             expect(langRange).toEqual('en-us');
         }));
-        it('should return en as language', inject(function(userLanguage) {
-            var lang = userLanguage.getLanguage("en-us");
+        it('should return en as language', inject(function (userLanguage) {
+            var lang = userLanguage.getLanguage('en-us');
             expect(lang).toEqual('en');
         }));
-        it('should return fr as language', inject(function(userLanguage) {
-            var lang = userLanguage.getLanguage("fr-be");
+        it('should return fr as language', inject(function (userLanguage) {
+            var lang = userLanguage.getLanguage('fr-be');
             expect(lang).toEqual('fr');
         }));
     });
 
-    describe('userLanguage service getLanguage function', function() {
-        it('should return en as language', inject(function(userLanguage) {
-            var lang = userLanguage.getLanguage("en-us");
+    describe('userLanguage service getLanguage function', function () {
+        it('should return en as language', inject(function (userLanguage) {
+            var lang = userLanguage.getLanguage('en-us');
             expect(lang).toEqual('en');
         }));
-        it('should return fr as language', inject(function(userLanguage) {
-            var lang = userLanguage.getLanguage("fr-be");
+        it('should return fr as language', inject(function (userLanguage) {
+            var lang = userLanguage.getLanguage('fr-be');
             expect(lang).toEqual('fr');
         }));
     });
