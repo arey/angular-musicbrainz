e0912b5353123496d0cf8cfa17f7e874fc1d4464
Antoine Rey <antoine.rey@gmail.com>
2014-02-03 22:13:45 +0100

    Format unit test to be compliant with jshint rules

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -2,29 +2,29 @@
 
 /* jasmine specs for directives go here */
 
-describe('directives', function() {
+describe('directives', function () {
 
     // Load the musicAlbumApp.directives module, which contains the directive
-  beforeEach(module('musicAlbumApp.directives'));
+    beforeEach(module('musicAlbumApp.directives'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
+    describe('app-version', function () {
+        it('should print current version', function () {
+            module(function ($provide) {
+                $provide.value('version', 'TEST_VER');
+            });
+            inject(function ($compile, $rootScope) {
+                var element = $compile('<span app-version></span>')($rootScope);
+                expect(element.text()).toEqual('TEST_VER');
+            });
+        });
     });
-  });
 
-    describe('rank directive', function() {
-        it('should print rating of 80%', function() {
+    describe('rank directive', function () {
+        it('should print rating of 80%', function () {
             module('directive-templates');
-            inject(function($templateCache, $compile, $rootScope) {
+            inject(function ($templateCache, $compile, $rootScope) {
                 var ratingTemplate = $templateCache.get('templates/rating.html');
-                $templateCache.put('templates/rating.html',ratingTemplate);
+                $templateCache.put('templates/rating.html', ratingTemplate);
 
                 // Compile a piece of HTML containing the directive
                 var element = $compile('<rank score="80"/>')($rootScope);
@@ -36,15 +36,15 @@ describe('directives', function() {
         });
     });
 
-    describe('cover directive', function() {
-        it('should display War cover album', function() {
+    describe('cover directive', function () {
+        it('should display War cover album', function () {
             module('directive-templates');
-            inject(function($templateCache, $compile, $rootScope) {
+            inject(function ($templateCache, $compile, $rootScope) {
                 var ratingTemplate = $templateCache.get('templates/cover.html');
-                $templateCache.put('templates/cover.html',ratingTemplate);
+                $templateCache.put('templates/cover.html', ratingTemplate);
 
                 $rootScope.album = {
-                    id : "c6b36664-7e60-3b3e-a24d-d096c67a11e9"
+                    id: 'c6b36664-7e60-3b3e-a24d-d096c67a11e9'
                 };
                 var element = $compile('<cover album-id="album.id"/>')($rootScope);
                 $rootScope.$digest();
